d080d771945369da7891656d6430a3250c59e821
mattiabonardi <mattiabonardi99@gmail.com>
2023-12-29 17:57:37 +0100

    feat: add toMap/fromMap session

--- types/session.go
+++ types/session.go
@@ -4,3 +4,15 @@ type Session struct {
 	Id       string `json:"id"`
 	Username string `json:"username"`
 }
+
+func (h *Session) fromMap(m map[string]string) {
+	h.Id = m["id"]
+	h.Username = m["username"]
+}
+
+func (h *Session) toMap() map[string]string {
+	return map[string]string{
+		"id":       h.Id,
+		"username": h.Username,
+	}
+}
